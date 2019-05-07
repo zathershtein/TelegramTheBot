aad52fe1a86776a8c71e58e0219546c4a1d7c28e
zathershtein <zathershtein@gmail.com>
2019-05-07 18:03:10 +0300

    Open file error handling

--- src/orange/index.ts
+++ src/orange/index.ts
@@ -1,5 +1,23 @@
 import * as fs from "fs";
 
-export function getTemp() {
-    return fs.readFileSync("/sys/devices/virtual/thermal/thermal_zone0/temp").readUIntBE(0, 3);
+export async function getTemp() {
+    let res: string;
+
+    await fs.readFile("./src/orange/testData",
+    (err, data) => {
+        if (err) {
+            if (err.code == 'ENOENT') {
+                console.error(err.message);
+            } else {
+                console.error(err);
+            }
+        } else {
+            console.log(data);
+        }
+        res = data.toString();
+    })
+    
+    return res;
 }
+
+// /sys/devices/virtual/thermal/thermal_zone0/temp
